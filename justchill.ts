dc1048a7d0f7d6945955f09e1a5b4288f3d34766
Marko Lukić <markolukicluk99@gmail.com>
2022-05-27 00:09:30 +0200

    Just some spaghetti code to chill

--- justchill.ts
+++ justchill.ts
@@ -1,6 +1,13 @@
 export class PotatoesBaby {
+
     hello = (message: string) => {
+        if(message) {
+            console.log(message);
+        }
+
+        return this.hello('Do not fail University man, you will regret it.');
 
     }
+
 }
 
