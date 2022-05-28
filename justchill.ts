96c8ec2b19f972232f8a90d78c1e0b590db12cae
Marko Lukić <markolukicluk99@gmail.com>
2022-05-28 18:06:09 +0200

    Useful method for counting larger numbers

--- justchill.ts
+++ justchill.ts
@@ -9,5 +9,17 @@ export class PotatoesBaby {
 
     }
 
+    /**
+     * While I was studying, I needed to figure out which number was larger,
+     * so I had to make this method
+     */
+    calcLargerNum = (number1: number, number2: number) => {
+        if(number1 > number2) {
+            return 'Number 1 is larger than number 2.'
+        } else {
+            return 'Number 2 is larger than number 1.'
+        }
+    }
+
 }
 
