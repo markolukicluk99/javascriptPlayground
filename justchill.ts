741a52d8bb01534ac4c859605f326fa24d746e49
Marko Lukić <markolukicluk99@gmail.com>
2022-08-02 12:19:38 +0200

    Remove cringe

--- justchill.ts
+++ justchill.ts
@@ -8,11 +8,7 @@ export class PotatoesBaby {
         return this.hello('Do not fail University man, you will regret it.');
 
     }
-
-    /**
-     * While I was studying, I needed to figure out which number was larger,
-     * so I had to make this method
-     */
+    
     calcLargerNum = (number1: number, number2: number) => {
         if(number1 > number2) {
             return 'Number 1 is larger than number 2.'
