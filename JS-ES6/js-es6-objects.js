793f62ba8bc57d40453f09128b680e34b2bed753
Marko Lukić <markolukicluk99@gmail.com>
2022-09-24 14:18:10 +0200

    ES6 - Object.assign

--- JS-ES6/js-es6-objects.js
+++ JS-ES6/js-es6-objects.js
@@ -9,3 +9,11 @@
  * Iterating through objects
  * Foreach loops
  */
+
+/**
+ * Adding values to a new object with Object.assign function
+ */
+ var objectPotato = {0: 'Potato1', 1: 'Potato2'};
+ var objectBanana = {0: 'Banana1', 1: 'Banana2'};
+ 
+ var final = Object.assign(objectPotato, objectBanana);
