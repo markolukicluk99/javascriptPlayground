b1227d68ea7591de31ca25f46d2338fa0aed9c6d
Marko Lukić <markolukicluk99@gmail.com>
2022-09-24 14:27:18 +0200

    ES6 - Cloning objects, and updating their values

--- JS-ES6/js-es6-objects.js
+++ JS-ES6/js-es6-objects.js
@@ -17,3 +17,12 @@
  var objectBanana = {0: 'Banana1', 1: 'Banana2'};
  
  var final = Object.assign(objectPotato, objectBanana);
+
+/**
+ * Cloning an old object, and forming a new one with Object.assign function
+ */
+var obj1 = {0: 123, 1: 345, 2: 678};
+// Make a new object with the same values of the previous one
+var cloneObj1 = Object.assign({}, obj1);
+// which alows us to update values inside the cloned object
+cloneObj1[0] = 567;
